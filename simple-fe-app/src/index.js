9e00c620995ba0a84dcd5e5a91b425d6a9932ad1
Stanislav Valkov <stanislavyv31@gmail.com>
2023-02-13 16:56:11 +0200

    Setup MUI

--- simple-fe-app/src/index.js
+++ simple-fe-app/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import CssBaseline from '@mui/material/CssBaseline';
+
 import App from './app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
+            <CssBaseline />
             <App />
         </Router>
     </React.StrictMode>
